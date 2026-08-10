4be3db7e675dafefa3004e6d6605f0c0984759d2
danikmarukovitch <marukovitch3@icloud.com>
2026-08-10 09:32:31 +0200

    Add authUserId field to User entity with Liquibase migration

--- src/main/java/org/example/internshipuserservice/entity/User.java
+++ src/main/java/org/example/internshipuserservice/entity/User.java
@@ -25,6 +25,9 @@ public class User extends Audit {
     @EqualsAndHashCode.Include
     private Long id;
 
+    @Column(name = "auth_user_id", nullable = false, unique = true)
+    private Long authUserId;
+
     @Column(nullable = false)
     @ToString.Include
     private String name;
